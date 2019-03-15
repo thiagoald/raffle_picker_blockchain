ea3aaf42c96efd351a9b9ecf609bf4b48d96d1d9
thiago <thiagoald@gmail.com>
2019-03-15 09:50:00 -0300

    Add script and requirements

---
+++ raffle_picker_blockchain.py
@@ -0,0 +1,21 @@
+from os.path import basename
+import sys
+import datetime
+from blockchain.blockexplorer import get_latest_block
+
+if __name__ == "__main__":
+    try:
+        number_of_raffles = int(sys.argv[1])
+    except:
+        print('Wrong arguments!')
+        print('Usage: {} number_of_raffles'.format(sys.argv[0]))
+        exit(1)
+    block = get_latest_block()
+    print('Block Time:\n')
+    print(datetime.datetime.fromtimestamp(block.time))
+    hash_str = block.hash
+    print('Hash (hex): {}'.format(hash_str))
+    hash_int = int(block.hash, 16)
+    print('Hash (int): {}'.format(hash_int))
+    hash_mod = hash_int%number_of_raffles
+    print('Hash (mod): {}'.format(hash_mod))
