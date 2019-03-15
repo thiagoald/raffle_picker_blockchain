d05e1c78401a42bf9195f210a2da684a422abb0a
thiago <thiagoald@gmail.com>
2019-03-15 09:58:05 -0300

    Fix formatting

--- raffle_picker_blockchain.py
+++ raffle_picker_blockchain.py
@@ -11,11 +11,12 @@ if __name__ == "__main__":
         print('Usage: {} number_of_raffles'.format(sys.argv[0]))
         exit(1)
     block = get_latest_block()
-    print('Block Time:\n')
+    print('Block Time:')
     print(datetime.datetime.fromtimestamp(block.time))
+    print('')
     hash_str = block.hash
     print('Hash (hex): {}'.format(hash_str))
     hash_int = int(block.hash, 16)
     print('Hash (int): {}'.format(hash_int))
     hash_mod = hash_int%number_of_raffles
-    print('Hash (mod): {}'.format(hash_mod))
+    print('Result (hash%{}): {}'.format(number_of_raffles, hash_mod))
